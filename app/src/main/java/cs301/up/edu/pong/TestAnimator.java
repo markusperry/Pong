5faa37b111335e4560fa4c76ae2baf1022655518
markusperry <perryma18@up.edu>
2016-02-26 13:19:14 -0800

    Added Comments

--- app/src/main/java/cs301/up/edu/pong/TestAnimator.java
+++ app/src/main/java/cs301/up/edu/pong/TestAnimator.java
@@ -13,6 +13,10 @@ import java.util.Random;
  * @author Steve Vegdahl
  * @author Andrew Nuxoll
  * @version February 2016
+ *
+ * @author Markus Perry
+ * @version Februar 2016 v.2
+ * edited by Markus to fit description.
  */
 public class TestAnimator implements Animator {
 
@@ -22,8 +26,8 @@ public class TestAnimator implements Animator {
     private boolean goBackwardsx = false; // whether clock is ticking backwards
     private boolean goBackwardsy = false;
     private int radius = 30;
-    public static boolean start = false;
-    public static int difficulty=1;
+    public static boolean start = false; //whether to start game or not
+    public static int difficulty=1; //paddle difficulty
 
     /**
      * Interval between animation frames: .03 seconds (i.e., about 33 times
@@ -52,21 +56,28 @@ public class TestAnimator implements Animator {
      * @param g the graphics object on which to draw
      */
     public void tick(Canvas g) {
+        //draw borders and paddle
         drawBorders(g);
         drawPaddle(difficulty,g);
+
+        //X and Y coordinate of the ball
         int xnum;
         int ynum;
+
+        //initial position of ball
         xnum = (g.getWidth()-100)/2;
         ynum = (g.getHeight()-100)/2;
 
+        //display startup and randomize directions
         if (!start)
         {
             drawGameText(g);
             randomizeDirs();
         }
+        //start the game
         if (start) {
 
-
+            //determine direction of animation
             if (goBackwardsx) {
                 countx--;
             }
@@ -80,20 +91,20 @@ public class TestAnimator implements Animator {
                 county++;
             }
 
+            //animate ball movement
             xnum += (countx * 30);
             ynum += (county * 30);
 
-
-
+            //wall bouncing
             if (xnum+radius > (g.getWidth()-50) || (xnum < 50+radius)) {
                 goBackwardsx = !goBackwardsx;
             }
-
             if (ynum < 50+radius)
             {
                 goBackwardsy=!goBackwardsy;
             }
 
+            //paddle bouncing based on difficulty
             if (ynum > g.getHeight() - (radius+65)) {
                 if (difficulty==1)
                 {
@@ -117,21 +128,13 @@ public class TestAnimator implements Animator {
                         start = false;
                     }
                 }
-
-
-
-
             }
-
-
-
+            //draw ball
             Paint redPaint = new Paint();
             redPaint.setColor(Color.RED);
             g.drawCircle(xnum, ynum, radius, redPaint);
-
-
         }
-    }
+    }//tick
 
     /**
      * Tells that we never pause.
@@ -158,6 +161,10 @@ public class TestAnimator implements Animator {
         return;
     }
 
+    /**
+     * draw the borders of the game
+     * @param g the canvas to draw on
+     */
     public void drawBorders(Canvas g) {
 
         Paint borderPaint = new Paint();
@@ -167,6 +174,10 @@ public class TestAnimator implements Animator {
         g.drawRect(g.getWidth() - 50, 0, g.getWidth(), g.getHeight(), borderPaint);
     }
 
+    /**
+     * generate a random number between 0 and 1
+     * @return a random number between 0 and 1
+     */
     public int genRandom() {
         Random gen = new Random();
         int random = gen.nextInt(2);
@@ -174,6 +185,9 @@ public class TestAnimator implements Animator {
         return random;
     }
 
+    /**
+     * Randomize the directions of the ball movement
+     */
     public void randomizeDirs()
     {
         int num = genRandom();
@@ -184,20 +198,32 @@ public class TestAnimator implements Animator {
         }
     }
 
+    /**
+     * Draw paddle based on difficulty
+     * @param diff the difficulty
+     * @param g the canvas to draw on
+     */
     public void drawPaddle(int diff, Canvas g)
     {
         Paint borderPaint = new Paint();
         borderPaint.setColor(Color.rgb(252, 236, 160));
+        //easy, draw big paddle
         if (diff==1)
         {
             g.drawRect(300,g.getHeight()-55,g.getWidth()-300,g.getHeight()-10,borderPaint);
         }
+
+        //hard, draw small paddle
         else
         {
             g.drawRect(500,g.getHeight()-55,g.getWidth()-500,g.getHeight()-10,borderPaint);
         }
     }
 
+    /**
+     * Draw the opening game text
+     * @param g the canvas to draw on
+     */
     public void drawGameText(Canvas g)
     {
         Paint wordPaint = new Paint();
@@ -206,6 +232,5 @@ public class TestAnimator implements Animator {
 
         g.drawText("PRESS START TO PLAY!",400f,g.getHeight()/2,wordPaint);
     }
-
 
 }//class TextAnimator
